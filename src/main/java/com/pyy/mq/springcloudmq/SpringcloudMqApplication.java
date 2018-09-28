517c57d0d2eeff154776660bc30ea4b9192612fe
wolf-mac <panyangyang@163.com>
2018-09-28 19:11:23 +0800

    项目初始化

---
+++ src/main/java/com/pyy/mq/springcloudmq/SpringcloudMqApplication.java
@@ -0,0 +1,12 @@
+package com.pyy.mq.springcloudmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudMqApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pyy/mq/springcloudmq/SpringcloudMqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pyy.mq.springcloudmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudMqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
